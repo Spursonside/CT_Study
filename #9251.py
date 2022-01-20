2c346f3939f5e095cc5219f1f3555e280801eb41
jun <djlee521@gmail.com>
2022-01-20 19:35:02 +0900

    DP 복습할 것

--- #9251.py
+++ #9251.py
@@ -1,11 +1,30 @@
 A = input()
 B = input()
+
 a, b = len(A), len(B)
 dp = [[0] * (b + 1) for _ in range(a + 1)]
-for i in range(1, b + 1):
-    for j in range(1, a + 1):
+
+for i in range(1, a + 1):
+    for j in range(1, b + 1):
         if A[i - 1] == B[j - 1]:
             dp[i][j] = dp[i - 1][j - 1] + 1
         else:
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 print(dp[a][b])
+
+"""
+import sys
+read = sys.stdin.readline
+
+word1, word2 = read().strip(), read().strip()
+h, w = len(word1), len(word2)
+cache = [[0] * (w+1) for _ in range(h+1)]
+
+for i in range(1, h+1):
+    for j in range(1, w+1):
+        if word1[i-1] == word2[j-1]:
+            cache[i][j] = cache[i-1][j-1] + 1
+        else:
+            cache[i][j] = max(cache[i][j-1], cache[i-1][j])
+print(cache[-1][-1])
+"""
