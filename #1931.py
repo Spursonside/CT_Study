d82aee40281b5c57381ad1000f83664a09133189
Jun <djlee521@gmail.com>
2022-01-25 01:27:26 +0900

    Greedy

--- #1931.py
+++ #1931.py
@@ -5,14 +5,16 @@ time = []
 for i in range(N):
     time.append(list(map(int, input().split())))
 
-time.sort(key=lambda x:x[0])
+time.sort(key=lambda x:(x[0], x[1]))
 
-memo = [[0, max(time, key = lambda x:x[1])]]
+memo = []
 
-for i in range(N):
-    if memo[-1][0] <= time[i][0] and memo[-1][1] >= time[i][1]:
+memo.append(time[0])
+
+for i in range(1, N):
+    if memo[-1][1] > time[i][1]:
+        memo[-1] = time[i]
+    elif memo[-1][1] <= time[i][0]:
         memo.append(time[i])
-    if memo[-1][1] <= time[i][0]:
-        memo.append(time[i])
 
-print(len(memo)-1)
+print(len(memo))
